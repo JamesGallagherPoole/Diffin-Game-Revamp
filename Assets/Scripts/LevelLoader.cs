fd79f2c4cd96da6cad46a423bd461a89a9ffffed
James Poole <jgaple@gmail.com>
2020-04-27 22:44:54 +0100

    Initial Transition

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Animator transition;
+    public StartGame startGameManager;
+
+    public float loadingLength = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartLoadingScreen()
+    {
+        Debug.Log("HEY!");
+        StartCoroutine(LoadingScreen());
+    }
+
+    IEnumerator LoadingScreen()
+    {
+        // Play Animation
+        transition.SetTrigger("Start");
+
+        // Wait for transition
+        yield return new WaitForSeconds(1);
+
+        // Wait on Loading
+        yield return new WaitForSeconds(loadingLength);
+
+        // Change Cameras
+        startGameManager.startGame();
+        // Play Animation
+        transition.SetTrigger("End");
+    }
+}
