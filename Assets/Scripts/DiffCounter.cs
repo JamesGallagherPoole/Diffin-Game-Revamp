e3bac799f09dc111446557ca392a66da291a981a
James Poole <jgaple@gmail.com>
2020-05-09 00:12:35 +0100

    add diff counter

---
+++ Assets/Scripts/DiffCounter.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiffCounter : MonoBehaviour
+{
+    public int diffCount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Load();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //When the Primitive collides with the walls, it will reverse direction
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(diffCount);
+        diffCount += 1;
+        Save();
+    }
+
+    private void Load()
+    {
+        if(PlayerPrefs.HasKey("diffCount"))
+        {
+            diffCount = PlayerPrefs.GetInt("diffCount");
+        }
+    }
+
+    private void Save()
+    {
+        PlayerPrefs.SetInt("diffCount", diffCount);
+        PlayerPrefs.Save();
+    }
+}
---
+++ Assets/Scripts/DisplayDiffCount.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DisplayDiffCount : MonoBehaviour
+{
+    public TextMeshProUGUI textComponent;
+    public DiffCounter diffCounter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        textComponent.text = diffCounter.diffCount.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void updateDiffCountDisplay() {
+        textComponent.text = diffCounter.diffCount.ToString();       
+    }
+}
